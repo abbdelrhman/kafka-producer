c59670a82a5e7237e8b4a9620a26583169b55f71
Hendawy <48328568+abbdelrhman@users.noreply.github.com>
2025-06-28 23:09:07 +0300

    just a change to test github desktop conflicts resolver

--- products/src/main/java/com/hendo/ws/products/dto/req/CreateProductRequest.java
+++ products/src/main/java/com/hendo/ws/products/dto/req/CreateProductRequest.java
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+// test // test
 import java.math.BigDecimal;
 
 @Data
