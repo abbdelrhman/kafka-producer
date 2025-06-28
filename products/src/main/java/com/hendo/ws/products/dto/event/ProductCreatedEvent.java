b77446fa2f2e94c4552a12ec9b41b4ed84cdd70e
Hendawy <abdohendawy99@gmail.com>
2025-06-28 23:12:13 +0300

    add docs for kafka event

--- products/src/main/java/com/hendo/ws/products/dto/event/ProductCreatedEvent.java
+++ products/src/main/java/com/hendo/ws/products/dto/event/ProductCreatedEvent.java
@@ -13,7 +13,7 @@ import java.math.BigDecimal;
 @Schema(description = "kafka event for product creation")
 public class ProductCreatedEvent {
     @Schema(description = "Product title", example = "iPhone 15 Pro", required = true)
-    private String titler;
+    private String title;
 
     @Schema(description = "Product price", example = "999.99", required = true)
     private BigDecimal price;
