db2495567451361415c23c211359761c4a5ac747
Hendawy <48328568+abbdelrhman@users.noreply.github.com>
2025-06-28 23:10:44 +0300

    dummy

--- products/src/main/java/com/hendo/ws/products/dto/event/ProductCreatedEvent.java
+++ products/src/main/java/com/hendo/ws/products/dto/event/ProductCreatedEvent.java
@@ -12,7 +12,7 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 public class ProductCreatedEvent {
     @Schema(description = "Product title", example = "iPhone 15 Pro", required = true)
-    private String title;
+    private String titler;
 
     @Schema(description = "Product price", example = "999.99", required = true)
     private BigDecimal price;
