d34abed5c4e539ee8e51b85215f8905dd427eb24
Hendawy <abdohendawy99@gmail.com>
2025-06-28 23:11:19 +0300

    Update ProductCreatedEvent.java

--- products/src/main/java/com/hendo/ws/products/dto/event/ProductCreatedEvent.java
+++ products/src/main/java/com/hendo/ws/products/dto/event/ProductCreatedEvent.java
@@ -10,6 +10,7 @@ import java.math.BigDecimal;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "kafka event for product creation")
 public class ProductCreatedEvent {
     @Schema(description = "Product title", example = "iPhone 15 Pro", required = true)
     private String title;
