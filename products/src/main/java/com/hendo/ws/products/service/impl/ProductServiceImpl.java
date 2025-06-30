15268544de49f03ee839a8b365cfd57c30e18e05
Hendawy <abdohendawy99@gmail.com>
2025-06-30 07:30:03 +0300

    making kafka sync
    how to make kafka call sync instead of async with the help of .join method

--- products/src/main/java/com/hendo/ws/products/service/impl/ProductServiceImpl.java
+++ products/src/main/java/com/hendo/ws/products/service/impl/ProductServiceImpl.java
@@ -5,8 +5,6 @@ import com.hendo.ws.products.dto.req.CreateProductRequest;
 import com.hendo.ws.products.dto.res.CreateProductResponse;
 import com.hendo.ws.products.mapper.ProductMapper;
 import com.hendo.ws.products.service.ProductService;
-
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,6 +14,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
+
 @Slf4j
 @Service
 public class ProductServiceImpl implements ProductService {
@@ -36,21 +35,33 @@ public class ProductServiceImpl implements ProductService {
         String productId = UUID.randomUUID().toString();
 
         // Map CreateProductRequest to ProductCreatedEvent using MapStruct
-        ProductCreatedEvent productCreatedEvent = productMapper.toProductCreatedEvent(createProductRequest,productId);
+        ProductCreatedEvent productCreatedEvent = productMapper.toProductCreatedEvent(createProductRequest, productId);
 
         // TODO: Add actual product creation logic here
         // TODO: Publish the ProductCreatedEvent to Kafka
-
-        CompletableFuture<SendResult<String,ProductCreatedEvent>> resultCompletableFuture = kafkaTemplate.send(productCreatedTopicName,productId,productCreatedEvent);
-        resultCompletableFuture.whenComplete((result,exception)->{
-           if (exception != null){
-               //fail
-               log.error("message failed with exception {} ", exception);
-           }else{
-               //success
-               log.info("message is sent successfully with metadata {}", result.getRecordMetadata());
-           }
+        //kafkaTemplate.send takes the following -> topicName, key, value
+        CompletableFuture<SendResult<String, ProductCreatedEvent>> resultCompletableFuture = kafkaTemplate.send(productCreatedTopicName, productId, productCreatedEvent);
+        resultCompletableFuture.whenComplete((result, exception) -> {
+            if (exception != null) {
+                //fail
+                log.error("message failed with exception");
+            } else {
+                //success
+                log.info("message is sent successfully with metadata {}", result.getRecordMetadata());
+            }
         });
+
+        /**
+         * if you need kafka call to be async just return the result
+         * if you need kafka call to by sync you can write the following line
+         */
+
+        /**
+         * this line blocks the current thread until the Kafka message is either successfully sent or an exception is thrown.
+         */
+        resultCompletableFuture.join();
+
+        log.info("***************** returning product id");
         return new CreateProductResponse(productId);
     }
 }
