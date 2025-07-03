b4b078ce07b37949a09fb0ff5143f76b6a7ebec7
Hendawy <abdohendawy99@gmail.com>
2025-07-03 16:49:19 +0300

    create a bean for kafka producer

--- products/src/main/java/com/hendo/ws/products/config/KafkaConfig.java
+++ products/src/main/java/com/hendo/ws/products/config/KafkaConfig.java
@@ -5,6 +5,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.context.annotation.Bean;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class KafkaConfig {
@@ -21,6 +28,27 @@ public class KafkaConfig {
     @Value("${kafka.topic.product-created.min-insync-replicas}")
     private String minInsyncReplicas;
 
+    @Value("${spring.kafka.producer.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Value("${spring.kafka.producer.key-serializer}")
+    private String keySerializer;
+
+    @Value("${spring.kafka.producer.value-serializer}")
+    private String valueSerializer;
+
+    @Value("${spring.kafka.producer.acks}")
+    private String acks;
+
+    @Value("${spring.kafka.producer.properties.delivery.timeout.ms}")
+    private String deliveryTimeoutMs;
+
+    @Value("${spring.kafka.producer.properties.linger.ms}")
+    private String lingerMs;
+
+    @Value("${spring.kafka.producer.properties.request.timeout.ms}")
+    private String requestTimeoutMs;
+
     @Bean
     NewTopic createTopic() {
         return TopicBuilder
@@ -30,4 +58,28 @@ public class KafkaConfig {
                 .config("min.insync.replicas", minInsyncReplicas)
                 .build();
     }
+
+    @Bean
+    public Map<String, Object> producerConfigs() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, keySerializer);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, valueSerializer);
+        props.put(ProducerConfig.ACKS_CONFIG, acks);
+        props.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, deliveryTimeoutMs);
+        props.put(ProducerConfig.LINGER_MS_CONFIG, lingerMs);
+        props.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, requestTimeoutMs);
+        return props;
+    }
+
+    @Bean
+    public ProducerFactory<String, Object> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfigs());
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
 }
